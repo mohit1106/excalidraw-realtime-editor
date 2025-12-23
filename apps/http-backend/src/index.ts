e8711d5cdfd3247c9a611389421b0b8bee049819
Mohit Verma <mohit51341862@gmail.com>
2025-12-24 03:27:47 +0530

    adding custom packages

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,17 +1,32 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
-
+import {CreateUserSchema, SigninSchema, CreateRoomSchema} from "@repo/common/types";
 const app = express();
 
 app.post("/signup", async (req, res) => {
-    
+    const data = CreateUserSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+    res.json({
+        userId: "123"
+    })
 })
 
 
 app.post("/signin", async (req, res) => {
-
+    const data = SigninSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
     const userId = 1;
     const token = jwt.sign({
         userId
@@ -24,7 +39,13 @@ app.post("/signin", async (req, res) => {
 
 
 app.post("/room", middleware, (req, res) => {
-    
+    const data = CreateRoomSchema.safeParse(req.body);
+    if(!data.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
 })
 
 app.listen(3001);
