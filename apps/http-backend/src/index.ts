fdb67877113c6ad7c4dcb0a77b71857d20336f44
Mohit Verma <mohit51341862@gmail.com>
2025-12-24 02:08:51 +0530

    excalidraw

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
+
+const app = express();
+
+app.post("/signup", async (req, res) => {
+    
+})
+
+
+app.post("/signin", async (req, res) => {
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET);
+
+    res.json({
+        token
+    })
+})
+
+
+app.post("/room", middleware, (req, res) => {
+    
+})
+
+app.listen(3001);
