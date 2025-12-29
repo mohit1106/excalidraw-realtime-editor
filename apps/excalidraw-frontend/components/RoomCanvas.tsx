f7a693e407b54452444d73984cf691de2172f502
Mohit Verma <mohit51341862@gmail.com>
2025-12-30 02:12:57 +0530

    draw shapes

--- apps/excalidraw-frontend/components/RoomCanvas.tsx
+++ apps/excalidraw-frontend/components/RoomCanvas.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { WS_URL } from "@/config";
 import { initDraw } from "@/draw";
 import { useEffect, useRef, useState } from "react";
 import { Canvas } from "./Canvas";
+import { WS_URL } from "@/config";
 
 export function RoomCanvas({roomId}: {roomId: string}) {
     const [socket, setSocket] = useState<WebSocket | null>(null);
 
     useEffect(() => {
-        const ws = new WebSocket(`${WS_URL}?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI3Njg0NDMwYy04YzNiLTRlZmQtOGFmNS00YzQwMzdmNjJkYzMiLCJpYXQiOjE3MzcyOTg2NjV9.xacFop0s231DoUVeLZormeIbBmIRaXftTVVI6weIqFo`)
+        const ws = new WebSocket(`${WS_URL}?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkMTc3YjhiYi1kOThmLTQxM2ItYWE5Ni0yMWVkZDM1MDhlY2QiLCJpYXQiOjE3NjY3MDk4MzR9.APZjeOXNCGoaZSm67-Y1csSUJKfFaGELqDIxNcbPEdM`)
 
         ws.onopen = () => {
             setSocket(ws);
